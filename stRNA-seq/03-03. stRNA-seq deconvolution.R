9f5e043cfba2265a8131824a2fcfc6a8821624de
deprodas <deprodasprotay@gmail.com>
2026-06-13 11:39:10 +0200

    Git commit after revision

--- stRNA-seq/03-03. stRNA-seq deconvolution.R
+++ stRNA-seq/03-03. stRNA-seq deconvolution.R
@@ -14,7 +14,7 @@ library(DOTr)
 # Visium data preparation 
 
 sample_dir <- "C:\\01. Research\\01. mIDH (hypoxia)\\02. Analysis\\03. Analysis (stRNA-seq)\\02. Spatial enrichment"
-st_visum <- readRDS(file.path(sample_dir, "MGH259.RDS"))
+st_visum <- readRDS(file.path(sample_dir, "BWH24.RDS"))
 st_count <- st_visum@assays$Spatial$counts 
 st_coord <- GetTissueCoordinates(st_visum)
 
@@ -33,7 +33,7 @@ class(sc_coord)
 dot.srt <- setup.srt(srt_data = st_count, srt_coords = st_coord) 
 dot.ref <- setup.ref(ref_data = sc_count, ref_annotations = sc_coord, ref_subcluster_size = 10) 
 dot_obj <- create.DOT(dot.srt, dot.ref)
-saveRDS(dot_obj, "MGH259_DOTr.RDS") 
+saveRDS(dot_obj, "BWH24_DOTr.RDS") 
 
 # Abundance weight: a larger value more closely matches the abundance of cell types in the spatial data to those in the reference data
 
@@ -51,7 +51,7 @@ plot_data$celltype <- colnames(dot_high.res@weights)[apply(dot_high.res@weights,
 p_basic <- ggplot(plot_data, aes(x = x, y = y, color = celltype)) +
   geom_point(size = 1.5) +
   theme_bw() 
-p_basic  
+p_basic 
 
 # ── Higher annotation ploting ───────────────────────────────────────────────── 
 
@@ -71,7 +71,7 @@ p_high_ann <- ggplot(plot_data, aes(x = x, y = y, color = group))+
         axis.title = element_blank(), 
         axis.ticks = element_blank()) 
 p_high_ann 
-ggsave(filename = "1. MGH259_surface plot all cells.pdf", plot = p_high_ann, width = 5.5, height = 4)
+ggsave(filename = "1. BWH24_surface plot all cells.pdf", plot = p_high_ann, width = 5.5, height = 4)
 
 # Add high resolution classification to spatial data  
 
@@ -81,11 +81,11 @@ st_visum <- AddMetaData(st_visum, metadata = plot_data$group, col.name = "ann_mp
 
 fraction_idh <- dot_high.res@weights %>% as.data.frame() 
 spatial_meta <- st_visum@meta.data
-st_merged_df <- cbind(fraction_idh, spatial_meta) 
+st_merged_df <- cbind(fraction_idh, spatial_meta)
+colnames(st_merged_df) 
 st_longgg_df <- st_merged_df %>%
   pivot_longer(cols = all_of(c('AC_like', 'OC_like', 'NPC_like', 'Cycling', 'Macrophage', 'Microglia', 'DCs', 'MDSC', 'Oligo', 'Endo', 'TCells', 'Pericytes')), 
-               names_to = 'Celltypes', 
-               values_to = 'Fraction')
+               names_to = 'Celltypes', values_to = 'Fraction')
 
 # Plotting cell fractions 
 
@@ -99,4 +99,44 @@ p_frac <- ggplot(st_longgg_df, aes(x = factor(Hypoxia_class), y = Fraction, fill
   stat_compare_means(comparisons = paircomparison) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))  
 p_frac
-ggsave(filename = "2. MGH259_fractions all cells.pdf", plot = p_frac, width = 4, height = 5)
+ggsave(filename = "2. BWH24_fractions all cells.pdf", plot = p_frac, width = 4, height = 5)
+
+# ── Define compartments / deconvoluted spots ────────────────────────────────── 
+
+# Marker geneset 
+
+lbl_genes <- read.csv("label_markers.csv")
+lbl_genes <- dput(as.character(lbl_genes$Gene))
+
+# Prepare data 
+
+Idents(object = st_visum) <- "ann_mps.mye_general" 
+st_no.img <- st_visum
+st_no.img@images <- list()
+st_immune <- subset(st_no.img, 
+                    subset = ann_mps.mye_general %in% c("Microglia", "Macrophage",  "MDSC", "Endo", "Oligo", "Pericytes", "TCells"))
+
+# Differential expression & heatmap 
+
+Idents(st_immune) <- "ann_mps.mye_general"
+table(Idents(st_immune))
+
+marker_st <- FindAllMarkers(st_immune, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+marker_cl <- marker_st %>%
+  group_by(cluster) %>%
+  slice_max(avg_log2FC, n = 100)
+
+top_genes <- marker_cl$gene
+gene_mark <- lbl_genes
+gene_post <- which(rev(unique(top_genes)) %in% gene_mark)
+gene_post
+
+DefaultAssay(st_immune) <- "SCT"
+st_immune <- ScaleData(st_immune, features = unique(top_genes))
+
+p_mark_ht <- DoHeatmap(st_immune, features = top_genes, size = 1) + 
+  scale_y_discrete(labels = function(x) ifelse(x %in% gene_mark, x, "")) +
+  geom_hline(yintercept = gene_post, linewidth = 0.5) +
+  NoLegend() 
+p_mark_ht
+ggsave(filename = "3. BWH24_markers.pdf", plot = p_mark_ht, width = 6, height = 6)
